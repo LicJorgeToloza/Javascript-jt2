9a644202a67dbac7a6be97bec2c0a58890f9bb57
licjorgetoloza <jorgetoloza.dg.ok@gmail.com>
2024-03-27 20:46:43 -0300

    modifico js

--- js/main2.js
+++ js/main2.js
@@ -7,8 +7,11 @@ function crearServicio (producto, areaServicio, clienteTipo, precio, pack) {
    this.pack = pack;
 }
 
+//Para crear un Combo de Servicios personalizado para determinado cliente
 const prodPersonalizado1 = new crearServicio ("Admin. de Redes Soc. Anual", "Marketing digital", "C", 750000, "Premium");
+console.log(prodPersonalizado1) ;
 
+//Array de Objetos
 const servicios = [
 	{
 		producto: "Diseño de Marca",
@@ -21,14 +24,14 @@ const servicios = [
         producto: "Diseño de Marca", 
         areaServicio: "Diseño Gráfico", 
         clienteTipo: "B", 
-        precio: 115000, 
+        precio: 114000, 
         pack: "Advance", 
      },
      { 
         producto: "Manual de Usos de Marca", 
         areaServicio: "Diseño Gráfico", 
         clienteTipo: "B", 
-        precio: 115000, 
+        precio: 119000, 
         pack: "Advance", 
      },
 	{
@@ -42,7 +45,7 @@ const servicios = [
 		producto: "Vectorizacion o Redibujo",
 		areaServicio: "Diseño Gráfico",
 		clienteTipo: "A",
-		precio: 70000,
+		precio: 77000,
 		pack: "Basic",
 	},
 	{ 
@@ -70,7 +73,7 @@ const servicios = [
         producto: "MD Redes 5PxS", 
         areaServicio: "Marketing Digital", 
         clienteTipo: "B", 
-        precio: 90000, 
+        precio: 93000, 
         pack: "Advance", 
      },
      { 
@@ -91,51 +94,34 @@ const servicios = [
         producto: "Contenido Redes A", 
         areaServicio: "Marketing Digital", 
         clienteTipo: "A", 
-        precio: 110000, 
+        precio: 112000, 
         pack: "Basic", 
      },
      { 
         producto: "Contenido Redes B", 
         areaServicio: "Marketing Digital", 
         clienteTipo: "B", 
-        precio: 110000, 
+        precio: 114000, 
         pack: "Advance", 
      },
      { 
         producto: "Contenido Redes C", 
         areaServicio: "Marketing Digital", 
         clienteTipo: "C", 
-        precio: 110000, 
+        precio: 116000, 
         pack: "Premium", 
      }
 ];
 
-console.log(servicios)
 
-//La idea es mostrar aca los "productos" u Objetos para despues por medio del prompt, el usuario pueda filtrar segun area
-function mostrarProductos(servDisenioJt){
-   servDisenioJt.forEach(prodJtDg => {
-       console.log(prodJtDg.producto + ' - ' + prodJtDg.areaServicio + ' - ' + prodJtDg.clienteTipo + ' - $' + prodJtDg.precio + prodJtDg.pack);
+//La idea es mostrar aca los "productos" u Objetos para despues por medio del prompt, el usuario pueda elegir un producto
+   servicios.forEach((prodJtDg) => {
+       console.log(prodJtDg.producto);
+       console.log(prodJtDg.precio);
+       console.log(prodJtDg.areaServicio);
+   });
 
-let areaServicio = prompt("Ingrese Area");
-
-function filtroArea(servicio){
-      if(producto){
-          return servicio.pack === pack
-      }
-      return servicio;
-}
-
-filtroArea()
+let servSeleccionado = prompt ("Ingrese Producto/Servicio a contratar");
+console.log(servicios.find((servicio) => servicio.producto === servSeleccionado));
 
 
-
-function filtroServ(){
-          const resultado = servicios.filter(filtroArea);
-          if(resultado.length>0){
-              mostrarProductos(resultado)
-          }else {
-              alert("El area ingresada no existe.")
-          }
-       }
-filtroServ()       
